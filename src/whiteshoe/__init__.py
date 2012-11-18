923f51ba8032f615a13a80eef17349d79893753b
Alastair Lynn <arplynn@gmail.com>
2012-11-18 05:24:50 +0000

    Make whiteshoe and whiteshoed not include too much
    Previously they would both include the whiteshoe package, which
    would load both the 'client' and 'server' modules. They now just
    load the client and server respectively.
    
    The reason for this change is that the client relies on the curses
    module which is not available in pypy. The server now runs in pypy.
    
    Signed-off-by: Jack Edge <yellowbounder@gmail.com>

--- src/whiteshoe/__init__.py
+++ src/whiteshoe/__init__.py
@@ -1,12 +1,12 @@
 from __future__ import print_function
 
-import client
-import server
-
-client_main = client.client_main
-server_main = server.server_main
-
 if __name__=='__main__':
+    import client
+    import server
+
+    client_main = client.client_main
+    server_main = server.server_main
+
     p = argparse.ArgumentParser()
     p.add_argument('-s','--server',action='store_true')
     namespace, remaining_args  = p.parse_known_args()
